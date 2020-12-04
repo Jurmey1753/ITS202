41fe61acf7d5ada059f17476df2b6ce1e8c5d08d
Jurmey1753 <tsheringjurmey74@gmail.com>
2020-12-04 10:40:22 -0500

    first commit

--- SequentialSearchST.java
+++ SequentialSearchST.java
@@ -1,205 +1,94 @@
-public class SequentialSearchST<Key, Value> {
-	Node head;
-	Node tail;
-	int size;
-    /**
-     * Initializes an empty symbol table.
-     */
-    public SequentialSearchST() {
-    	head = null;
-    	tail = null;
-    	size = 0;
-    }
-    class Node{
-    	private Key key;
-    	private Value value;
-    	private Node next;
-    	public Node(Key key, Value value, Node next) {
-    		this.key = key;
-    		this.value = value;
-    		this.next = next;
-    	}
-    	public Key getKey() {
-    		return key;
-    	}
-    	public Value getValue() {
-    		return value;
-    	}
-    	public Node getNext() {
-    		return next;
-    	}
-    	public void setKey(Key n) {
-    		key = n;
-    	}
-    	public void setValue(Value n) {
-    		value = n;
-    	}
-    	public void setNext(Node n) {
-    		next = n;
-    	}
-//		public String toString() {
-//			return key + " - " + value;
-//		}
-    }
+import java.util.ArrayList;
+public class SequentialSearchST<Key, Value>
+{
+    private int n;
+    private Node first;
 
-    /**
-     * Returns the number of key-value pairs in this symbol table.
-     *
-     * @return the number of key-value pairs in this symbol table
-     */
-    public int size() {
-    	return size;
-    }
+    private class Node
+    {
+        private Key key;
+        private Value val;
+        private Node next;
 
-    /**
-     * Returns true if this symbol table is empty.
-     *
-     * @return {@code true} if this symbol table is empty;
-     *         {@code false} otherwise
-     */
-    public boolean isEmpty() {
-    	if(size == 0) {
-    		return true;
-    	}
-    	return false;
-    }
-
-    /**
-     * Returns true if this symbol table contains the specified key.
-     *
-     * @param  key the key
-     * @return {@code true} if this symbol table contains {@code key};
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public boolean contains(Key key) {
-    	if(key == null) {
-    		throw new IllegalArgumentException ("The key doesnot have value");
-    	}
-    	else {
-    		return get(key) != null;
-    	}
-    }
-
-    /**
-     * Returns the value associated with the given key in this symbol table.
-     *
-     * @param  key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *     and {@code null} if the key is not in the symbol table
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public Value get(Key key) {
-    	if(key == null) {
-    		throw new IllegalArgumentException("The key doesnot have value");
-    	}
-    	for(Node i = head; i != null; i = i.next) {
-    		if(key == i.key) {
-    			return i.value;
-    		}
-    	}
-    	return null;
-    }
-
-    /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old 
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
-     * if the specified value is {@code null}.
-     *
-     * @param  key the key
-     * @param  val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public void put(Key key, Value val) {
-        if (key == null) {
-             System.out.println("Key cannot be null");
-             return;
+        public Node(Key key, Value val, Node next)
+        {
+            this.key = key;
+            this.val = val;
+            this.next = next;
         }
-        Node newest = new Node(key, val, null);
+    }    
 
-        if (head == null) {
-            head = newest;
-            size ++;
-            
+    public SequentialSearchST()
+    {
+    
+    }
+    public int size()
+    {
+        return n;
+    
+    }
+    public boolean isEmpty() 
+    {
+        return size()==0;
+    }
+    public boolean contains(Key key) 
+    {
+        if(key==null) throw new IllegalArgumentException("Argument to contain() is null");
+        return get(key) != null;    
+    }
+    public Value get(Key key) 
+    {
+        if (key == null) throw new IllegalArgumentException("argument to get() is null"); 
+        for (Node x = first; x != null; x = x.next) 
+        {
+            if (key.equals(x.key))
+                return x.val;
         }
-        else if (head.next==null || head.next!=null) {
-                for (Node x = head; x!=null; x=x.next) {
-                    if (key == x.key) {
-                        x.value = val;
-                        return;
-                    }
-                            
-                } 
-                newest.next = head;
-                head = newest;
-                size++;       
+        return null;
+    }
+    public void put(Key key, Value val) 
+    {
+        if (key == null) throw new IllegalArgumentException("first argument to put() is null"); 
+        if (val == null) 
+        {
+            delete(key);
+            return;
         }
 
+        for (Node x = first; x != null; x = x.next) 
+        {
+            if (key.equals(x.key)) 
+            {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
+        n++;
+   
     }
-    /**
-     * Removes the specified key and its associated value from this symbol table     
-     * (if the key is in this symbol table).    
-     *
-     * @param  key the key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
-    public void delete(Key key) {
-    	 if (key == null) {
-    		 throw new IllegalArgumentException("argument to delete() is null"); 
-    	 }
-    	 else {
-    		 head = delete(head, key);
-    	 }
+    public void delete(Key key) 
+    {
+        if (key == null) throw new IllegalArgumentException("argument to delete() is null"); 
+        first = delete(first, key);
     }
+    private Node delete(Node x, Key key) 
+    {
+        if (x == null) return null;
+        if (key.equals(x.key)) 
+        {
+            n--;
+            return x.next;
+        }
+        x.next = delete(x.next, key);
+        return x;
+    }
+    public Iterable<Key> keys()
+    {
+        ArrayList<Key> arr = new ArrayList<Key>();
+        for (Node x = first; x != null; x = x.next)
+            arr.add(x.key);
+        return arr;
 
-    // delete key in linked list beginning at Node x
-    // warning: function call stack too large if table is large
-    private Node delete(Node x, Key key) {
-    	if(x == null) {
-    		return null;
-    	}
-    	else if(key == x.key) {
-    		
-    		size = size - 1;
-    		return x.next;
-    	}
-    	else {
-    		x.next = delete(x.next, key);
-    		return x;
-    	}
-    }
-
-    /**
-     * Returns all keys in the symbol table as an {@code Iterable}.
-     * To iterate over all of the keys in the symbol table named {@code st},
-     * use the for each notation: {@code for (Key key : st.keys())}.
-     *
-     * @return all keys in the symbol table
-     */
-    public void traversal(Node head) {
-    	Node curNode = head;
-    	while(curNode != null) {
-    		System.out.println(curNode.key + " has a value " + curNode.value);
-    		curNode = curNode.getNext();
-    	}
-    }
-    /**
-         main method
-     */
-    public static void main(String[] args) {
-        SequentialSearchST <Integer, String> obj = new SequentialSearchST<Integer, String>();
-        obj.put(90, "A");
-        obj.put(91, "B");
-        obj.put(92, "C");
-        obj.put(93, "D");
-        obj.put(94, "E");
-        obj.delete(94);
-        assert(obj.size == 4);
-        assert(obj.contains(91) == true);
-        assert(obj.contains(99) == false);
-        assert(obj.isEmpty() == false);
-        obj.traversal(obj.head);
-        System.out.println("All test are Correct");
     }
 }
